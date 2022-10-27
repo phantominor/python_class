8196b88afde25b0681e0c2eaecb3c89ebb3c2af7
phantominor <109658618+phantominor@users.noreply.github.com>
2022-10-27 16:23:20 +0800

    Update Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -17,3 +17,11 @@
 #      ***
 #       *
 
+def hollow_diamond(n):
+    for row in range(1, 2*n):
+        if row <=2 or row >= 2*n-2:
+            print(' '*(max(n-row,row-n)) + '*'*(2*min(row,2*n-row)-1) + ' '*(max(n-row,row-n)-1))
+        else:
+            print(' '*(max(n-row,row-n)) + '**' + ' '*(2*min(row,2*n-row)-5) + '**' + ' '*(max(n-row,row-n)-1))
+
+hollow_diamond(3)
