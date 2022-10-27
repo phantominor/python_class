fde40c921ac1f3353fc29ba8558173b8645bd2ec
danfwd123 <73151214+danfwd123@users.noreply.github.com>
2022-10-27 10:10:59 +0800

    Update Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -27,3 +27,16 @@ This diamond has double `*` boundary.
  ***
   *
 ```
+
+
+"""Solution"""
+
+def hollow_diamond(n):
+    for i in range(2*n-1):
+        i = i if i<n else 2*n-2-i
+        if i == 0:
+            print(" "*(n-1)+"*"+" "*(n-1))
+        elif i == 1:
+            print(" "*(n-2)+"***"+" "*(n-2))
+        else:
+            print(" "*(n-1-i)+"**"+ " "*(2*i-3)+"**"+" "*(n-1-i))
