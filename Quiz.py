1502517fb9e87166f8d791ea913cf86cc1737850
phantominor <motnahpminusone@gmail.com>
2022-12-08 12:24:23 +0800

    answer

--- Quiz.py
+++ Quiz.py
@@ -11,21 +11,41 @@ atp3 = pd.read_csv('https://query.data.world/s/kgdeqxaao7crmxw4uix4twly6euq4p') 
 atp4 = pd.read_csv('https://query.data.world/s/t3ub55t6xxydoxdwtfpuo2if54fzfc') # US
 mylist = [atp1, atp2, atp3, atp4,]
 
-# Please count the number of times player A and player B have faced off (A v.s. B) in the championship match. Show the top eight frequent match pairs and use horizontal bar to visualize the result. Plot and save the figure.
+# Please count the number of times player A and player B have faced off (A v.s. B) in the championship match. 
+# Show the top nine frequent match pairs and use horizontal bar to visualize the result. Plot and save the figure.
 # PS: the order of the names does not matter (A v.s. B equals B v.s.A).
 
 def quiz(mylist):
+    dual = {}
+    for j in mylist:
+        for i in range(len(j)):
+            name1 = j['Champion'][i]
+            name2 = j['Runner-up'][i]
+            set1 = str(name1)+' vs '+str(name2)
+            set2 = str(name2)+' vs '+str(name1)
+            if set1 in dual:
+                dual[set1] += 1
+            elif set2 in dual:
+                dual[set2] += 1
+            else:
+                dual[set1] = 1
+    result = pd.Series(dual)
+    result = result.sort_values(ascending = False)
+    result = result[:9]
+    result = result.iloc[::-1]
     '''
     write your code here
     prepare two list: wins and name
     '''
     plt.figure(facecolor = 'w',edgecolor= 'w' ,figsize=(10,5)) 
-    plt.barh(np.arange(8), wins, color=list('ccbbbgrr'))
-    plt.yticks(np.arange(8), name, fontsize=15)
+    plt.barh(np.arange(9), result.values, color=list('ccbbbgrr'))
+    plt.yticks(np.arange(9), result.index, fontsize=15)
     plt.xticks(np.arange(10), fontsize=15)
     plt.title('match count', fontsize=15)
     plt.grid()
     plt.tight_layout()
-    plt.savefig("./quiz.png")
+    # plt.savefig("./quiz.png")
+    plt.show()
     plt.close()
     
+quiz(mylist)
